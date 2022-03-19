61791a27b437edc274009dd6d8655191338aa67d
Fabrizio Altera <Fabrizioaltera1404@gmail.com>
2022-03-19 14:07:34 -0300

    Java

---
+++ main.js
@@ -0,0 +1,72 @@
+/* let nombreUsuario = prompt ("Ingresar Nombre de Usuario");
+
+if(nombreUsuario == ""){
+    alert ("No ingresaste un Nombre de Usuario");
+} else{
+    alert ("Nombre de Usuario creado " + nombreUsuario);
+}
+
+let contraseña = prompt ("Ingresar Contraseña")
+
+if (contraseña == "12345678"){
+    alert ("No puedes utilizar números de corrido")
+}else {
+    alert ("Nueva contraseña creada " + nombreUsuario)
+}
+ if (contraseña=="asdfghjk "){
+    alert ("Debe contener una Mayuscula y un Numero")
+} else{
+    alert ("Nueva contraseña creada " + nombreUsuario)
+}
+if (contraseña == "Altera01"){
+        alert ("Nueva contraseña creada " + nombreUsuario)
+    }
+    >
+    for(let i = 1; i <= 10; i += 1){
+        console.log (i);
+    }
+
+ */
+
+
+
+
+/* 
+  let metodoPago = prompt (  "Ingrese el metodo de pago");
+
+  switch (metodoPago.toLowerCase()){
+      case "efectivo":
+          console.log ("Elegiste efectivo");
+          break;
+       case "debito":
+          console.log("Elegiste Tarjeta de debito");
+          break;
+       case "cheque":
+           console.log("Elegiste cheque")
+           break;
+           case "bitcoin":
+           console.log ("Elegiste Bitcoin")
+           break;
+           default:
+               console.log("Elegiste otro medio de pago")
+
+          
+  } */
+
+  /* let cuotas = prompt ("Ingrese las cuotas a pagar " );
+   total=($3000)
+   switch (cuotas.toLowerCase){
+       case "3 cuotas":
+           document.write ("Elegiste el pago en 3 cuotas" + ${ ( total / 3 ) + 10%} );
+           break;
+        case "6 cuotas";
+            document.write ("Elegiste el pago en 6 cuotas" + ${ ( total / 6 ) + 18%});
+            break;
+        case "12 cuotas";
+            document.write ("Elegiste el pago en 12 cuotas" + ${ ( total / 12 ) + 25%});
+            break;       
+   }  */
+
+
+ /*   function (1 ,cuotas = "3 cuotas de " + precio/3);
+   console.log */
